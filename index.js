ee751eaaad5cd3851806bde420901dcff6548cf5
Tahrin Maruf <txm273@gmail.com>
2020-06-12 15:26:55 -0230

    added database connection, can retrieve data from db

---
+++ index.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+
+const mysql = require('mysql');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+//port
+const port = process.env.PORT || 8082;
+app.listen(port, () => {
+  console.log('REST API listening on port ', port);
+});
+
+//test routes 1
+app.get('/', async (req, res) => {
+  res.json({
+    status: 'API is ready to serve printers from ' + process.env.PORT,
+  });
+});
+
+//test routes 2
+app.route('/printers').get(function (req, res, next) {
+  connection.query('SELECT * FROM `printers` ', function (
+    error,
+    results,
+    fields
+  ) {
+    if (error) throw error;
+    res.json(results);
+  });
+});
+
+//connection
+let connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASS,
+});
+connection.connect(function (err) {
+  if (err) {
+    console.error('Error connecting: ' + err.stack);
+    return;
+  }
+  console.log('Connected as thread id: ' + connection.threadId);
+});
